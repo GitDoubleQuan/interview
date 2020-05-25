217d371136b60979482fc740d17b9a21a13554ad
double <wxsq1991>
2020-05-25 15:21:48 +0800

    commit 20200525

--- flink/src/main/scala/tableapi/TableApiTest.scala
+++ flink/src/main/scala/tableapi/TableApiTest.scala
@@ -7,6 +7,7 @@ import org.apache.flink.table.api.scala._
 import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, TableEnvironment}
 import org.apache.flink.table.descriptors.{Csv, FileSystem, Kafka, OldCsv, Schema}
 import org.apache.flink.types.Row
+import pojo.SensorReading
 
 object TableApiTest {
   def main(args: Array[String]): Unit = {
@@ -57,6 +58,7 @@ object TableApiTest {
       )
       .createTemporaryTable("kafkaInputTable")
 
+
     //3查询操作
     //3.1简单聚合操作
     val table = blinkStreamTableEnv.from("inputTable")
@@ -71,6 +73,29 @@ object TableApiTest {
         |where id = 'sensor_1'
         |group by id
         |""".stripMargin)
+
+    //4.table、view和dataStream之间的转换
+    //4.1dataStream to table
+    val source = env
+      .readTextFile("/Users/double/workspace/interview/flink/src/main/resources/tempdata")
+      .map(data => {
+        val arr = data.split(",")
+        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+      })
+
+
+    //4.2基于dataStream创建表
+    //后面也可不跟schema，默认一一对应，如果跟了可以做字段的重命名  字段的位置调整  删除字段等操作
+    val sourceTable = blinkStreamTableEnv.fromDataStream(source, 'id, 'temperature as 'temp, 'timestamp as 'ts)
+    sourceTable.printSchema()
+    //4.3基于dataStream创建view,跟创建表一样，schema可以不加，默认一一对应
+    blinkStreamTableEnv.createTemporaryView("source_view",source,'id, 'temperature as 'temp, 'timestamp as 'ts)
+    //4.4基于table创建view
+    blinkStreamTableEnv.createTemporaryView("source_view2",sourceTable)
+
+
+
+
 
     blinkStreamTableEnv.toRetractStream[(String, Double)](table).print()
     env.execute()
